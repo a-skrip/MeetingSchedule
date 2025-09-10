80a0385b48c3e78a3645b2b344d73c20eaea19c4
a-skrip <alex-himki@mail.ru>
2025-09-10 11:11:20 +0300

    добавлены комменты

--- src/MeetingSchedule.java
+++ src/MeetingSchedule.java
@@ -6,20 +6,22 @@ import java.util.Locale;
 
 public class MeetingSchedule {
     public static void main(String[] args) {
-
+        //Создание даты начала периода
         LocalDate startDate = LocalDate.of(2050, Month.MARCH, 20);
         DateTimeFormatter formatterForWeekDay = DateTimeFormatter.ofPattern("dd.MM.yyyy - EEEE ");//.withLocale(Locale.ENGLISH);
-
         System.out.println("Начальная дата: " + startDate.format(formatterForWeekDay));
 
+        //Записать в переменную ближайший четверг от даты начала
         LocalDate nextThursday = findNearestThursdayFrom(startDate);
         System.out.println("Ближайший четверг: " + nextThursday.format(formatterForWeekDay));
 
+        //Записать дату окончания периода
         LocalDate endOfEvents = startDate
                 .plusMonths(1)
                 .plusWeeks(2);
         System.out.println("Конец периода встреч: " + endOfEvents.format(formatterForWeekDay));
 
+        //Вывод дат всех запланированных встреч
         LocalDate nextMeetingAt = nextThursday;
         while (nextMeetingAt.isBefore(endOfEvents)) {
             System.out.println("Встреча: " + nextMeetingAt.format(formatterForWeekDay));
